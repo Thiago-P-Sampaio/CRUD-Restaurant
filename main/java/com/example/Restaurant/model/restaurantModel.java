6e403057ad5cfde6e560b344ab7c9977f0eb4019
unknown <kauamarquesrodrigues423@gmail.com>
2024-10-25 10:58:57 -0300

    Model

---
+++ main/java/com/example/Restaurant/model/restaurantModel.java
@@ -0,0 +1,47 @@
+package com.example.Restaurant.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name="foods")
+@AllArgsConstructor
+@NoArgsConstructor
+public class restaurantModel {
+
+	@Id
+	private UUID Id;
+	private String nome;
+	private String desc;
+	private float valor;
+	public UUID getId() {
+		return Id;
+	}
+	public void setId(UUID id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+}
